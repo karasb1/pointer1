21b9eb6941ee9a3481c472485411b8e148043f09
karasb1 <bohdankaras456@gmail.com>
2024-09-21 14:24:08 +0300

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-
+	int* a = new int;
+	int* b = new int;
+	cin >> *a;
+	cin >> *b;
+	cout << *a << "--->";
+	int c = *a;
+	*a = *b;
+	cout << *a << endl;
+	cout << *b << "--->";
+	*b = c;
+	cout << *b << endl;
 	return 0;
 }
