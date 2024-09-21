f6d6eda60acfebd70c81e6bbf652fdbc5e37cd84
karasb1 <bohdankaras456@gmail.com>
2024-09-21 17:12:18 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
+
+int* create_array(int* size)
+{
+	int* arr = new int[*size];
+	for (int i = 0; i < *size; i++)
+	{
+		arr[i] = rand() % 50;
+	}
+	return arr;
+}
+void print_array(int* arr, int* size)
+{
+	for (int i = 0; i < *size; i++)
+	{
+		cout << *(arr + i) << " ";
+	}
+	cout << endl;
+}
+void reverse(int* arr, int* size)
+{
+	for (int i = 0; i < *size; i++)
+	{
+		cout << arr[*size - 1 - i] << " ";
+	}
+}
+
+
+
+
 
 int main()
 {
-	
+	srand(time(0));
+	int* size = new int;
+	cout << "Enter the size of the array: ";
+	cin >> *size;
+	int* arr = create_array(size);
+	print_array(arr, size);
+	reverse(arr, size);
+
 	return 0;
 }
