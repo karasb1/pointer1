03411d14c8f29fbd3077b24be245c24df58075ca
karasb1 <bohdankaras456@gmail.com>
2024-09-21 16:45:47 +0300

    The ex4 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,31 @@ using namespace std;
 
 int main()
 {
-	
+	int* first_number = new int;
+	int* second_number = new int;
+	string* op = new string;
+	cout << "Enter the first number: ";
+	cin >> *first_number;
+	cout << "Enter the second number: ";
+	cin >> *second_number;
+	cout << "Enter the operation: ";
+	cin >> *op;
+	if (*op == "+")
+	{
+		cout << *first_number + *second_number;
+	}
+	else if (*op == "-")
+	{
+		cout << *first_number - *second_number;
+	}
+	else if (*op == "/")
+	{
+		cout << *first_number / *second_number;
+	}
+	else if (*op == "*")
+	{
+		cout << *first_number * *second_number;
+	}
+
 	return 0;
 }
